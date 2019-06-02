a2105bfb739d9b5fc883d837b7e1bcf28db49078
Marcel Härtel <m.haertel@gmx.net>
2019-06-02 11:20:06 +0200

    Facepunch.Steamworks init

---
+++ Facepunch.Steamworks MonoGame Integration/SteamworksIntegration.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Steamworks;
+using System;
+
+namespace Facepunch.Steamworks_MonoGame_Integration
+{
+    public class SteamworksIntegration : Game
+    {
+        private readonly GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        public SteamworksIntegration()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            try
+            {
+                SteamClient.Init(480);
+            }
+            catch (System.Exception e)
+            {
+                Console.Out.WriteLine(e.InnerException);
+                Exit();
+            }
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            
+            base.Draw(gameTime);
+        }
+    }
+}
